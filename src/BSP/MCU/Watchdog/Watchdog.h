c6f90e104ea3f3476a94beadf5c6b0926145f441
gsieffert <GSieffert@ems-ch.com>
2015-09-04 10:04:16 +0200

    Add the Watchdog Software Unit to the MCU Package part of the BSP to the Configuration manager

---
+++ src/BSP/MCU/Watchdog/Watchdog.h
@@ -0,0 +1,18 @@
+/*
+ * File:		wdog.h
+ * Purpose:     Provide common watchdog module routines
+ *
+ * Notes:
+ */
+
+#ifndef __WDOG_H__
+#define __WDOG_H__
+
+/********************************************************************/
+
+// function prototypes
+void Watchdog_Disable(void);
+void Watchdog_Unlock(void);
+
+/********************************************************************/
+#endif /* __WDOG_H__ */
