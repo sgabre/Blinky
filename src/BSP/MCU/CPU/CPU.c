2c4b64ee8a68dd7d5b1fa60bd41d267480dac4f3
gsieffert <GSieffert@ems-ch.com>
2015-09-04 12:13:33 +0200

    Add the CPU Software Unit to the MCU Package part of the BSP to the configuration manager

---
+++ src/BSP/MCU/CPU/CPU.c
@@ -0,0 +1,13 @@
+/*
+ * CPU.c
+ *
+ *  Created on: 8 janv. 2015
+ *      Author: gsi
+ */
+#include "BSP/Common/Common.h"
+
+#include "BSP/MCU/CPU/CPU.h"
+
+void CPU_Setup(void)
+{
+}
---
+++ src/BSP/MCU/CPU/CPU.h
@@ -0,0 +1,13 @@
+/*
+ * CPU.h
+ *
+ *  Created on: 8 janv. 2015
+ *      Author: gsi
+ */
+
+#ifndef CPU_H_
+#define CPU_H_
+
+void CPU_Setup(void);
+
+#endif /* CPU_H_ */
