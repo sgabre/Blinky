f4da01ea7a61baf952fda5ac91268b130ae44428
gsieffert <GSieffert@ems-ch.com>
2015-09-10 13:07:57 +0200

    Update CPu Software Unit

--- src/BSP/MCU/CPU/CPU.c
+++ src/BSP/MCU/CPU/CPU.c
@@ -9,7 +9,65 @@
 #include "BSP/MCU/CPU/CPU.h"
 #include "BSP/MCU/MCG/MCG.h"
 
+/* Global variables */
+volatile uint8_t SR_reg;               /* Current value of the FAULTMASK register */
+volatile uint8_t SR_lock = 0x00U;      /* Lock */
+
+void Cpu_SetBASEPRI(uint32_t Level);
+
+/*
+** ===================================================================
+**     Method      :  Cpu_SetBASEPRI (component MK20DX256LL7)
+**
+**     Description :
+**         This method sets the BASEPRI core register.
+**         This method is internal. It is used by Processor Expert only.
+** ===================================================================
+*/
+void Cpu_SetBASEPRI(uint32_t Level)
+{
+	__asm ("msr basepri, %[input]"::[input] "r" (Level):);
+}
+
+/*
+** ===================================================================
+**     Method      :  PE_low_level_init (component MK20DX256LL7)
+**
+**     Description :
+**         Initializes beans and provides common register initialization. 
+**         The method is called automatically as a part of the 
+**         application initialization code.
+**         This method is internal. It is used by Processor Expert only.
+** ===================================================================
+*/
 void CPU_Setup(void)
 {
-	MCG_Setup();
+	//MCG_Setup();
+
+	/* Initialization of the SIM module */
+  /* PORTA_PCR4: ISF=0,MUX=7 */
+  //PORTA_PCR4 = (uint32_t)((PORTA_PCR4 & (uint32_t)~(uint32_t)(PORT_PCR_ISF_MASK)) | (uint32_t)(PORT_PCR_MUX(0x07)));
+
+  /* Initialization of the RCM module */
+  /* RCM_RPFW: RSTFLTSEL=0 */
+  //RCM_RPFW &= (uint8_t)~(uint8_t)(RCM_RPFW_RSTFLTSEL(0x1F));
+  /* RCM_RPFC: RSTFLTSS=0,RSTFLTSRW=0 */
+  //RCM_RPFC &= (uint8_t)~(uint8_t)(RCM_RPFC_RSTFLTSS_MASK |RCM_RPFC_RSTFLTSRW(0x03));
+
+  /* Initialization of the FTFL_FlashConfig module */
+  /* Initialization of the PMC module */
+  /* PMC_LVDSC1: LVDACK=1,LVDIE=0,LVDRE=1,LVDV=0 */
+  //PMC_LVDSC1 = (uint8_t)((PMC_LVDSC1 & (uint8_t)~(uint8_t)(PMC_LVDSC1_LVDIE_MASK |PMC_LVDSC1_LVDV(0x03))) | (uint8_t)(PMC_LVDSC1_LVDACK_MASK |PMC_LVDSC1_LVDRE_MASK));
+  /* PMC_LVDSC2: LVWACK=1,LVWIE=0,LVWV=0 */
+  //PMC_LVDSC2 = (uint8_t)((PMC_LVDSC2 & (uint8_t)~(uint8_t)(PMC_LVDSC2_LVWIE_MASK |PMC_LVDSC2_LVWV(0x03))) | (uint8_t)(PMC_LVDSC2_LVWACK_MASK));
+  /* PMC_REGSC: BGEN=0,ACKISO=0,BGBE=0 */
+  //PMC_REGSC &= (uint8_t)~(uint8_t)(PMC_REGSC_BGEN_MASK |PMC_REGSC_ACKISO_MASK |PMC_REGSC_BGBE_MASK);
+  /* SMC_PMPROT: ??=0,??=0,AVLP=0,??=0,ALLS=0,??=0,AVLLS=0,??=0 */
+  //SMC_PMPROT = 0x00U;                  /* Setup Power mode protection register */
+
+	/* Common initialization of the CPU registers */
+  /* NVICIP20: PRI20=0 */
+  //NVICIP20 = NVIC_IP_PRI20(0x00);
+  /* Enable interrupts of the given priority level */
+  //Cpu_SetBASEPRI(0U);
 }
--- src/BSP/MCU/CPU/CPU.h
+++ src/BSP/MCU/CPU/CPU.h
@@ -1,12 +1,67 @@
-/*
- * CPU.h
- *
- *  Created on: 8 janv. 2015
- *      Author: gsi
- */
-
 #ifndef CPU_H_
 #define CPU_H_
+// Active configuration define symbol
+#define PEcfg_FLASH 1U
+
+// Methods configuration constants - generated for all enabled component's methods
+
+// Events configuration constants - generated for all enabled component's events
+#define Cpu_OnNMIINT_EVENT_ENABLED
+
+#define CPU_BUS_CLK_HZ                  20971520U /* Initial value of the bus clock frequency in Hz */
+#define CPU_CORE_CLK_HZ                 20971520U /* Initial value of the core/system clock frequency in Hz.  */
+
+#define CPU_CLOCK_CONFIG_NUMBER         0x01U /* Specifies number of defined clock configurations. */
+
+#define CPU_BUS_CLK_HZ_CLOCK_CONFIG0    20971520U /* Value of the bus clock frequency in the clock configuration 0 in Hz. */
+#define CPU_CORE_CLK_HZ_CLOCK_CONFIG0   20971520U /* Value of the core/system clock frequency in the clock configuration 0 in Hz. */
+
+
+#define CPU_INT_SLOW_CLK_HZ             32768U /* Value of the slow internal oscillator clock frequency in Hz  */
+#define CPU_INT_FAST_CLK_HZ             4000000U /* Value of the fast internal oscillator clock frequency in Hz  */
+
+#define CPU_FAMILY_Kinetis             /* Specification of the core type of the selected cpu */
+#define CPU_DERIVATIVE_MK20DX256LL7    /* Name of the selected cpu derivative */
+#define CPU_PARTNUM_MK20DX256VLL7      /* Part number of the selected cpu */
+#define CPU_LITTLE_ENDIAN              /* The selected cpu uses little endian */
+
+/* CPU frequencies in clock configuration 0 */
+#define CPU_CLOCK_CONFIG_0              0x00U /* Clock configuration 0 identifier */
+#define CPU_CORE_CLK_HZ_CONFIG_0        20971520UL /* Core clock frequency in clock configuration 0 */
+#define CPU_BUS_CLK_HZ_CONFIG_0         20971520UL /* Bus clock frequency in clock configuration 0 */
+#define CPU_FLEXBUS_CLK_HZ_CONFIG_0     10485760UL /* Flexbus clock frequency in clock configuration 0 */
+#define CPU_FLASH_CLK_HZ_CONFIG_0       10485760UL /* FLASH clock frequency in clock configuration 0 */
+#define CPU_USB_CLK_HZ_CONFIG_0         0UL /* USB clock frequency in clock configuration 0 */
+#define CPU_PLL_FLL_CLK_HZ_CONFIG_0     20971520UL /* PLL/FLL clock frequency in clock configuration 0 */
+#define CPU_MCGIR_CLK_HZ_CONFIG_0       32768UL /* MCG internal reference clock frequency in clock configuration 0 */
+#define CPU_OSCER_CLK_HZ_CONFIG_0       0UL /* System OSC external reference clock frequency in clock configuration 0 */
+#define CPU_ERCLK32K_CLK_HZ_CONFIG_0    1000UL /* External reference clock 32k frequency in clock configuration 0 */
+#define CPU_MCGFF_CLK_HZ_CONFIG_0       32768UL /* MCG fixed frequency clock */
+
+
+typedef struct  {
+  uint32_t cpu_core_clk_hz;            /* Core clock frequency in clock configuration */
+  uint32_t cpu_bus_clk_hz;             /* Bus clock frequency in clock configuration */
+  uint32_t cpu_flexbus_clk_hz;         /* Flexbus clock frequency in clock configuration */
+  uint32_t cpu_flash_clk_hz;           /* FLASH clock frequency in clock configuration */
+  uint32_t cpu_usb_clk_hz;             /* USB clock frequency in clock configuration */
+  uint32_t cpu_pll_fll_clk_hz;         /* PLL/FLL clock frequency in clock configuration */
+  uint32_t cpu_mcgir_clk_hz;           /* MCG internal reference clock frequency in clock configuration */
+  uint32_t cpu_oscer_clk_hz;           /* System OSC external reference clock frequency in clock configuration */
+  uint32_t cpu_erclk32k_clk_hz;        /* External reference clock 32k frequency in clock configuration */
+  uint32_t cpu_mcgff_clk_hz;           /* MCG fixed frequency clock */
+} TCpuClockConfiguration;
+
+/* The array of clock frequencies in configured clock configurations */
+extern const TCpuClockConfiguration PE_CpuClockConfigurations[CPU_CLOCK_CONFIG_NUMBER];
+
+
+
+/* Global variables */
+/*lint -esym(765,SR_reg) Disable MISRA rule (8.10) checking for symbols (SR_reg). The SR_reg is used in inline assembler. */
+extern volatile uint8_t SR_reg;        /* Current FAULTMASK register */
+/*lint -esym(765,SR_lock) Disable MISRA rule (8.10) checking for symbols (SR_lock). The SR_reg is used in inline assembler. */
+extern volatile uint8_t SR_lock;
 
 void CPU_Setup(void);
 
