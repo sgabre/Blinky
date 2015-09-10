1bbf14a3c632cba883424502f4917c8c6b5d1238
gsieffert <GSieffert@ems-ch.com>
2015-09-10 13:07:23 +0200

    Add the Print library redefinition to trace software unit

--- src/BSP/Drivers/Trace/Trace.h
+++ src/BSP/Drivers/Trace/Trace.h
@@ -7,7 +7,21 @@
 #ifndef TRACE_H_
 #define TRACE_H_
 
+#ifdef TRACE_ON
+#include <stdlib.h>
+#include "Library/std/io.h"
+#ifdef IAR
+#include "intrinsics.h"
+#endif
+#endif
+
+
 /* Public Macros Definition */
+#ifdef TRACE_ON
+#define TRACE printf
+#else
+#define TRACE
+#endif
 
 /* Public Constants Definition ============================================= */
 
@@ -22,11 +36,8 @@
 /* Public variables  Definition ============================================ */
 
 /* Public Functions Declaration  =========================================== */
-#ifdef TRACE_ON
-#define TRACE printf
-#else
-#define TRACE
-#endif
+
+
 
 void Trace_Setup(void);
 void TraceLong(uint8 type, uint32 value);
